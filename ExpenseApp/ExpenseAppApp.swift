1f22651101f8034d13eeddb8b905dafa23e5a001
Leon Gell <lgell@lcs.on.ca>
2023-06-01 15:21:18 -0400

    Initial Commit

---
+++ ExpenseApp/ExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseAppApp.swift
+//  ExpenseApp
+//
+//  Created by Leon Gell on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
